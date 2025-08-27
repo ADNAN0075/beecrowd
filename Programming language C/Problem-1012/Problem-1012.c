6761ac04a21480de2cde446d5db142fdc24169ce
Adnan Al Sami <adoradnanalsami@gmail.com>
2025-08-27 11:37:13 +0600

    Create Problem-1012.c

---
+++ Programming language C/Problem-1012/Problem-1012.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float A,B,C;
+    double pi,rectangled;
+    scanf("%f",&A);
+    scanf("%f",&B);
+    Scanf("%f",&C);
+    rectangled = 0.5*A*C;
+    printf("TRIANGULO: %.2lf",rectangled);
+
+    return 0;
+}
